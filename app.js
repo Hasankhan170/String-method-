8ce17e6c1494aa6ac7c2d0220d8d4d9c7fffbec1
Hasankhan170 <hasankhanali0316@gmail.com>
2024-04-30 01:49:04 -0700

    string method

--- app.js
+++ app.js
@@ -9,3 +9,31 @@
 
 // DONE 
 
+
+// Q : 2
+
+// const head = document.querySelector("h2")
+// const userValue = prompt("enter your fav mobile model");
+// const add = userValue.length;
+
+// head.innerHTML = "MY FAV MOBILE PHONE IS :" + " " +userValue+ " " +add;
+
+// DONE 
+
+
+// Q : 3
+
+// const head = document.querySelector("h2")
+
+// const word = prompt("")
+// const add = word.indexOf("n");
+
+// head.innerHTML = word+ " " + add;
+
+
+// DONE 
+
+
+// Q : 3
+
+
